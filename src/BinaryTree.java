04bd446b2f05b026cae38d06d6571dc0de958912
Adam <adam.moataz.hindawi2014@gmail.com>
2026-02-02 18:23:53 +0200

    first commit

---
+++ src/BinaryTree.java
@@ -0,0 +1,15 @@
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class BinaryTree {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
